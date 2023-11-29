4ebbc12efb82e0d96579f46c41ae70713596ebba
Bohdan Vlasenko <118814052+sky-tech-bv@users.noreply.github.com>
2023-11-29 14:21:51 +0000

    Debuging

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 */
 package main
 
-import "github.com/sky-tech-bv/PDC/Week-3/kbot/cmd"
+import "github.com/sky-tech-bv/devops-course/cmd"
 
 func main() {
 	cmd.Execute()
